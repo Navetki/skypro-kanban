196d97b543a38d05eec4fb452649782df8b59d72
Navetki <zhannik-86@mail.ru>
2026-02-01 23:00:14 +0300

    Домашка по теме"Роутинг"

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import "./App.css";
-
 import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 import { GlobalStyle } from "./GlobalStyle.styled";
 import * as S from "./App.styled";
-
-import PopUser from "./components/popups/PopUser/PopUser";
-import PopNewCard from "./components/popups/PopNewCard/PopNewCard";
-import PopBrowse from "./components/popups/PopBrowse/PopBrowse";
-
-import MainPage from "./pages/MainPage/MainPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import AppRoutes from "./components/AppRoutes/AppRoutes";
 
 function App() {
   const [user, setUser] = useState(true);
@@ -21,19 +11,7 @@ function App() {
     <>
       <GlobalStyle />
       <S.Wrapper>
-        <Routes>
-          <Route
-            path="/"
-            element={user ? <MainPage /> : <Navigate to="/login" />}
-          >
-            <Route path="task/:id" element={<PopBrowse />} />
-            <Route path="exit" element={<PopUser setUser={setUser} />} />
-            <Route path="new-card" element={<PopNewCard />} />
-          </Route>
-          <Route path="/login" element={<LoginPage setUser={setUser} />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <AppRoutes user={user} setUser={setUser} />
       </S.Wrapper>
     </>
   );
